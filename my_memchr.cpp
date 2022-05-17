fbd84c496b6b068243637838145b824e461f7066
heesu <hee1su123@naver.com>
2022-05-17 19:43:23 +0900

    memchr & memset edit

--- my_memchr.cpp
+++ my_memchr.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
@@ -13,6 +14,31 @@ using namespace std;
 // value : Value to be located. The value is passed as an int, but the function performs a byte per byte search using the unsigned char conversion of this value.
 // num : Number of bytes to be analyzed. size_t is an unsigned integral type.
 
-void * my_memchr (void* ptr, int value, size_t num) {
-    
+// value 에 해당하는 값이 *ptr 에서 몇번째에 처음 나오는지 확인 후 주소를 반환
+
+void *my_memchr(void *ptr, int value, size_t num)
+{
+    char *ret;
+    ret = (char*)ptr;
+    for (int i = 0; i < num; i++) {
+        if (*ret == (char)value) {
+            return ret;
+        }
+        else
+            ret++;
+    }
+    return NULL;
+}
+
+int main()
+{
+    char *pch;
+    char *pch2;
+    char str[] = "Example string";
+    pch = (char *)memchr(str, 'p', strlen(str));
+    pch2 = (char *)my_memchr(str, 'p', strlen(str));
+    printf("'p' found at position %ld.\n", pch - str + 1);
+    printf("'p' found at position %ld.\n", pch2 - str + 1);
+
+    return 0;
 }
--- my_memset.cpp
+++ my_memset.cpp
@@ -15,23 +15,59 @@ using namespace std;
 
 // Return Value : ptr
 
-void* my_memset(void* ptr, int value, size_t num) {
-    char* ret;
+// *ptr 에서 num개의 바이트를 value 값으로 바꾼다.
+
+void *my_memset(void *ptr, int value, size_t num)
+{
+    char *ret;
     char val = value;
 
-	ret = (char*)ptr;
-	for (int i = 0; i < num; i++) {
+    ret = (char *)ptr;
+    for (int i = 0; i < num; i++)
+    {
         ret[i] = val;
     }
-	return ret;
+    return ret;
 }
 
-int main() {
+// 특이 케이스
+// 1. num 의 크기가 ptr 의 길이보다 긴 경우
+// 2. num = 0 인 경우
+// 3. C언어의 경우 문자열 마지막에 들어가야 할 '/0' 고려
+
+int main()
+{
+    // Case1
     char str[] = "almost every programmer should know memset!";
     char str2[] = "almost every programmer should know memset!";
     memset(str, '-', 6);
     my_memset(str2, '-', 6);
-    puts(str);
-    puts(str2);
+    cout << str << endl;
+    cout << str2 << endl;
+
+    // Case2
+    char str3[] = "abc";
+    char str4[] = "abc";
+    memset(str3, '-', 3);
+    my_memset(str3, '-', 3);
+    cout << str3 << endl;
+    cout << str4 << endl;
+
+    // Case3
+    char str5[] = "abc";
+    char str6[] = "abc";
+    memset(str5, '-', 4);
+    my_memset(str6, '-', 4);
+    cout << str5 << endl;
+    cout << str6 << endl;
+
+    // Case4
+    char str7[] = "abc";
+    char str8[] = "abc";
+    memset(str7, '-', 0);
+    my_memset(str8, '-', 0);
+    cout << str7 << endl;
+    cout << str8 << endl;
+
     return 0;
 }
