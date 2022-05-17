13b607b9e0df394856210af54583b6b5dc7d1196
heesu <hee1su123@naver.com>
2022-05-17 20:32:04 +0900

    memcmp

--- my_memcmp.cpp
+++ my_memcmp.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
@@ -13,6 +14,32 @@ using namespace std;
 // ptr2 : Pointer to block of memory.
 // num : Number of bytes to compare.
 
+// 두개의 메모리를 비교. ptr1 가 가리키는 num 바이트의 데이터와 ptr2 가 가리키는 num 바이트의 데이터를 비교해 같으면 0, dest 가 더 크면 양수, src 가 크면 음수를 반환한다.
+
+
 int my_memcmp (const void* ptr1, const void* ptr2, size_t num) {
-    
+    char *comp1 = (char*)ptr1;
+    char *comp2 = (char*)ptr2;
+
+    for (int i = 0; i < num; i++) {
+        if (comp1[i] != comp2[i]) {
+            return comp1[i] - comp2[i];
+        }
+    }
+    return 0;
+}
+
+int main()
+{
+    char buffer1[] = "DWgaOtP12df0";
+    char buffer2[] = "DWGAOTP12DF0";
+
+    int n;
+    int m;
+
+    n = memcmp ( buffer1, buffer2, sizeof(buffer1) );
+    m = my_memcmp ( buffer1, buffer2, sizeof(buffer1) );
+    cout << "memcmp: " << n << ", " << "my_memcmp: " << m << endl;
+
+    return 0;
 }
